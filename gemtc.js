da4bedc645d8d38121bcd6ab84249c01ea1d96f5
Connor Stroomberg <c.stroomberg@gmail.com>
2015-05-28 14:52:44 +0200

    setup for login

--- gemtc.js
+++ gemtc.js
@@ -1,11 +1,14 @@
 var express = require('express'),
+  session = require('express-session'),
+  bodyParser = require('body-parser'),
+  cookieParser = require('cookie-parser'),
   everyauth = require('everyauth');
 
 everyauth.google
   .appId('100331616436-dgi00c0mjg8tbc06psuhluf9a2lo6c3i.apps.googleusercontent.com')
   .appSecret('9ROcvzLDuRbITbqj-m-W5C0I')
   .scope('https://www.googleapis.com/auth/userinfo.profile email')
-  .handleAuthCallbackError( function (req, res) {
+  .handleAuthCallbackError(function(req, res) {
     // If a user denies your app, Google will redirect the user to
     // /auth/facebook/callback?error=access_denied
     // This configurable route handler defines how you want to respond to
@@ -14,26 +17,39 @@ everyauth.google
     // view notifying the user that their authentication failed and why.
     console.log('gemtc.handleAuthCallbackError');
   })
-  .findOrCreateUser( function (session, accessToken, accessTokenExtra, googleUserMetadata) {
+  .findOrCreateUser(function(session, accessToken, accessTokenExtra, googleUserMetadata) {
     // find or create user logic goes here
     // Return a user or Promise that promises a user
     // Promises are created via
     //     var promise = this.Promise();
     console.log('find or create useer');
-  })
-  .redirectPath('/');
+    return {
+      "username": googleUserMetadata.name,
+      "firstName": googleUserMetadata.given_name,
+      "lastName": googleUserMetadata.family_name
+    };
+  }).redirectPath('/');
 
 var app = express();
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(cookieParser('mr ripley'));
 app
-.use(express.static('app'))
-.use(everyauth.middleware(app));
+  .use(express.static('app'))
+  .use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+  }))
+  .use(everyauth.middleware());
 
 app.get("/", function(req, res, next) {
-  if (req.user) {
-  	console.log('user found');
+  if (req.session.auth && req.session.auth.loggedIn) {
+    console.log('user found');
     res.redirect('/projects.html');
   } else {
-  	console.log('user not found');
+    console.log('user not found');
     res.redirect('/signin.html');
 
   }
