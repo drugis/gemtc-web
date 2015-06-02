636bbd26e4b5cd494b824fa97c70f7f8a75ac4fc
Daan Reid <osmosisch@gmail.com>
2015-06-02 13:21:07 +0200

    add selenium/nightwatch tests

---
+++ test/integration/gemtctest.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'Gemtc attract mode' : function (browser) {
+    browser
+      .url('http://localhost:3000')
+      .waitForElementVisible('body', 1000)
+      .pause(1000)
+      .assert.containsText('h1', 'gemtc.org')
+      .end();
+  }
+};
