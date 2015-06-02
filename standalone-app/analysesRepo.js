19c972ed6889d61baa21764dbf43f829188f7d05
Daan Reid <osmosisch@gmail.com>
2015-06-02 17:22:14 +0200

    route

--- standalone-app/analysesRepo.js
+++ standalone-app/analysesRepo.js
@@ -2,12 +2,12 @@ var db = require('./db');
 
 module.exports = {
   get: function(analysisId, callback) {
-    db.query('SELECT * FROM analysis WHERE ID=$1', analysisId, function(err, result) {
+    db.query('SELECT * FROM analysis WHERE ID=$1', [analysisId], function(err, result) {
       callback(err, result)
     });
   },
   query: function() {
-   db.query('SELECT * FROM analysis WHERE OWNER=$1', ownerId, function(err, result) {
+   db.query('SELECT * FROM analysis WHERE OWNER=$1', [ownerId], function(err, result) {
       callback(err, result)
     });
   }
--- standalone-app/analysesRouter.js
+++ standalone-app/analysesRouter.js
@@ -1,22 +1,25 @@
+var logger = require('./logger');
 var express = require('express');
 var analysesRepo = require('./analysesRepo');
 
 var router = express.Router();
 
 router
-  .route('/analyses/:analysisId')
-  .get(function(request, response, next) {
-    if(request.params.analysisId) {
-      analysesRepo.get(request.params.analysisId, function(err, analysis) {
-        response.json(JSON.stringify(analysis[0]));
-        next();
-      });
-    } else {
-      analysesRepo.query(ownerId, function(analyses) {
-        response.json(analysis);
-        next();
-      });
-    }
+  .get('/', function(request, response, next) {
+    logger.debug('get id-less analyses');
+    analysesRepo.query(request.session.auth.google.user.id, function(analyses) {
+      response.json(analysis);
+      next();
+    });
+  })
+  .get('/:analysisId', function(request, response, next) {
+    logger.debug('get analysis with id ' + request.params.analysisId);
+    analysesRepo.get(request.params.analysisId, function(err, analysis) {
+      response.json(JSON.stringify(analysis[0]));
+      next();
+    });
   });
+;
+
 
 module.exports = router;
