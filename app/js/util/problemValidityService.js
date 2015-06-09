5dc820bc0d8b1a7d4ea9ef44d4286ef6700c80a3
Connor Stroomberg <c.stroomberg@gmail.com>
2015-06-09 15:10:25 +0200

    check is file is valid string before checking checking problem

--- app/js/util/problemValidityService.js
+++ app/js/util/problemValidityService.js
@@ -20,7 +20,7 @@ define(['angular', 'lodash'], function(angular, _) {
 
     function areColumnsConsistent(entries) {
       // no such thing as proper array/set equals until ecma6
-      var firstColumnProperties =  _.keys(entries[0]).sort().join('');
+      var firstColumnProperties = _.keys(entries[0]).sort().join('');
 
       return !_.find(entries, function(entry) {
         return _.keys(entry).sort().join('') !== firstColumnProperties;
@@ -49,37 +49,59 @@ define(['angular', 'lodash'], function(angular, _) {
     function getValidity(problem) {
       var result = {
         isValid: true,
-        messages: []
+        message: ""
       };
 
       if (!problem || problem.length === 0) {
         result.isValid = false;
-        result.messages.push('The problem file is empty.');
+        result.message += ' The problem file is empty.';
       } else {
         if (!problem.entries) {
           result.isValid = false;
-          result.messages.push('The problem does not contain a list of entries')
+          result.message += ' The problem does not contain a list of entries';
         } else if (!areEntriesValid(problem)) {
           result.isValid = false;
-          result.messages.push('The entries must be a list of data rows; Each data row must contain at least the study and treatment columns');
-        } else if(!areColumnsConsistent(problem.entries)) {
+          result.message += ' The entries must be a list of data rows, each data row must contain at least the study and treatment columns';
+        } else if (!areColumnsConsistent(problem.entries)) {
           result.isValid = false;
-          result.messages.push('Each entry must have the same data columns');
+          result.message += ' Each entry must have the same data columns';
         }
 
         if (!problem.treatments) {
           result.isValid = false;
-          result.messages.push('The problem does not contain a list of treatments')
+          result.message += ' The problem does not contain a list of treatments';
         } else if (!areTreatmentsValid(problem.treatments)) {
           result.isValid = false;
-          result.messages.push('The treatments field must contain a list of objects that all have name and id')
+          result.message += ' The treatments field must contain a list of objects that all have name and id';
         }
       }
       return result;
     }
 
+    function isValidJsonObjectAsString(inputString) {
+      var isValidJsonString = inputString && ((typeof inputString) === 'string') && /^[\],:{}\s]*$/.test(
+        inputString.replace(/\\["\\\/bfnrtu]/g, '@')
+        .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+        .replace(/(?:^|:|,)(?:\s*\[)+/g, '')
+      );
+
+      if(isValidJsonString) {
+        return {
+          isValid: true
+        };
+      } else {
+        return {
+          isValid: false,
+          message: 'The file does not containt a valid json object'
+        };
+      }
+
+ 
+    }
+
     return {
-      getValidity: getValidity
+      getValidity: getValidity,
+      isValidJsonObjectAsString: isValidJsonObjectAsString
     };
   };
   return dependencies.concat(ProblemValidityService);
