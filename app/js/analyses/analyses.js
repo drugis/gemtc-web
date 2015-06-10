b5230366671a21d29deb92150c09f0dff6d1c58a
Daan Reid <osmosisch@gmail.com>
2015-06-10 13:55:54 +0200

    load models module as well in tests

--- app/js/analyses/analyses.js
+++ app/js/analyses/analyses.js
@@ -2,7 +2,7 @@
 
 define(function (require) {
   var angular = require('angular');
-  var dependencies = ['ngResource'];
+  var dependencies = ['ngResource', 'gemtc.models'];
 
   return angular.module('gemtc.analyses', dependencies)
     // controllers
