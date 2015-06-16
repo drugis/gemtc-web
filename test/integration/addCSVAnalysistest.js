3bb018234bd42cf8ce821e499aaac71531b8da93
Daan Reid <osmosisch@gmail.com>
2015-06-16 15:35:46 +0200

    refactor integration tests

--- test/integration/addCSVAnalysistest.js
+++ test/integration/addCSVAnalysistest.js
@@ -1,28 +1,18 @@
-var login = require('./util/login.js');
+var login = require('./util/login');
+var AnalysesPage = require('./analyses/analysesPage');
 
-// test the login
+var analysisTitle = 'my title';
 
 module.exports = {
-  "gemtc login test" : function (browser) {
-    login(browser, 'http://localhost:3001')
-      .waitForElementVisible('body', 15000)
-      .waitForElementVisible('#analyses-header', 15000)
-      .assert.containsText('#analyses-header', 'Analyses')
-      .waitForElementVisible('#add-analysis-btn', 15000)
-      .click('#add-analysis-btn')
-      .waitForElementVisible('#add-analysis-form', 15000)
-      .setValue('#title-input', 'my analysis title')
-      .pause(1000)
-      .setValue('#outcome-input', 'my analysis outcome')
-      .pause(1000)
-      .click('#add-analysis-form button')
-      .setValue('#problem-file-upload', require('path').resolve(__dirname + '/example.csv'))
-      .pause(1000)
-      .assert.elementPresent('#submit-add-analysis-btn')
-      .click('#submit-add-analysis-btn')
-       .waitForElementVisible('#analysis-header', 15000)
-      .assert.containsText('#analysis-header', 'Analysis')
-      .pause(1000)
-      .end();
+  "gemtc login test": function(browser) {
+    var analysesPage = new AnalysesPage(browser);
+    login(browser, 'http://localhost:3001');
+
+    analysesPage.waitForPageToLoad();
+    analysesPage.addAnalysis(analysisTitle, 'my outcome', '/example.csv');
+    browser
+      .waitForElementVisible('#analysis-header', 10000)
+      .assert.containsText('dd', analysisTitle);
+    analysesPage.end();
   }
 };
