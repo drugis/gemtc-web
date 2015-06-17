7735e6ef1c8ae809365f6a7b44289708629f4236
Daan Reid <osmosisch@gmail.com>
2015-06-17 12:52:55 +0200

    changesets for new iteration

---
+++ changesets/create-database-change-set-1.sql
@@ -0,0 +1,47 @@
+
+CREATE TABLE Account (id SERIAL NOT NULL,
+  email VARCHAR,
+  name VARCHAR NOT NULL,
+  firstName VARCHAR NOT NULL,
+  lastName VARCHAR NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- table to hold auth2 details
+CREATE TABLE UserConnection (tokenId VARCHAR NOT NULL,
+  googleUserId VARCHAR(255) NOT NULL,
+  accessToken VARCHAR(255) NOT NULL,
+  expireTime BIGINT,
+  refreshToken VARCHAR(255),
+  tokenType VARCHAR(255) NOT NULL,
+  userId INT NOT NULL,
+  PRIMARY KEY (tokenId),
+  FOREIGN KEY(userId) REFERENCES Account(id)
+);
+
+CREATE TABLE Analysis (id SERIAL NOT NULL,
+  owner INT NOT NULL,
+  title VARCHAR NOT NULL,
+  outcome varchar(255) NOT NULL,
+  problem JSONB NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY(owner) REFERENCES Account(id)
+);
+
+CREATE TABLE Model (
+  id SERIAL NOT NULL,
+  title VARCHAR NOT NULL,
+  analysisId INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY(analysisId) REFERENCES Analysis(id)
+);
+
+CREATE TABLE PataviTask (
+  id SERIAL NOT NULL,
+  modelId INT NOT NULL,
+  method varchar,
+  problem JSONB,
+  result TEXT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(modelId) REFERENCES Model(id)
+);
