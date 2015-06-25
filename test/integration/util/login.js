bc40e88bf0e2ca2e998e523b44128588d0ac3422
Connor Stroomberg <c.stroomberg@gmail.com>
2015-06-25 16:27:16 +0200

    make integration test more robust by making accept step option in google login prodecure

--- test/integration/util/login.js
+++ test/integration/util/login.js
@@ -1,17 +1,21 @@
-module.exports = function(browser, url){
-    return browser
-      .url(url)
-      .waitForElementVisible('body', 50000)
-      .click('button[type="submit"]')
-      .waitForElementVisible('body', 50000)
-      .assert.containsText('h2', 'Sign in with your Google Account')
-      .pause(1000)
-      .setValue('input[type=email]', 'addistestuser1@gmail.com')
-      .click('input[type="submit"]')
-      .pause(1000)
-      .setValue('input[type=password]', 'speciaalvoordejenkins')
-      .click('#signIn')
-      // no need to approve approve acces each time
-      // .pause(3000) // wait for submit button to become active (thanks for keeping us safe google)
-      // .click('#submit_approve_access')
+module.exports = function(browser, url) {
+  browser
+    .url(url)
+    .waitForElementVisible('body', 50000)
+    .click('button[type="submit"]')
+    .waitForElementVisible('body', 50000)
+    .assert.containsText('h2', 'Sign in with your Google Account')
+    .pause(1000)
+    .setValue('input[type=email]', 'addistestuser1@gmail.com')
+    .click('input[type="submit"]')
+    .pause(1000)
+    .setValue('input[type=password]', 'speciaalvoordejenkins')
+    .click('#signIn')
+    .pause(3000) // wait for submit button to become active (thanks for keeping us safe google)
+  browser.isVisible('#submit_approve_access', function(result) {
+    if (result.value === true) {
+      browser.click('#submit_approve_access')
+    }
+  });
+  return browser;
 };
