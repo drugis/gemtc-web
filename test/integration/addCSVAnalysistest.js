bed7de7f24a3f9b6f6e8ca71a6a1442ed3fc5825
Connor Stroomberg <c.stroomberg@gmail.com>
2015-06-25 16:33:16 +0200

    fix integration test ( CSVAnalysistest and JSONAnalysis) by updating test to reflect interface changes

--- test/integration/addCSVAnalysistest.js
+++ test/integration/addCSVAnalysistest.js
@@ -2,6 +2,7 @@ var login = require('./util/login');
 var AnalysesPage = require('./analyses/analysesPage');
 
 var analysisTitle = 'my title';
+var analysisOutcomeTitle = 'my outcome';
 
 module.exports = {
   "gemtc login test": function(browser) {
@@ -9,10 +10,11 @@ module.exports = {
     login(browser, 'http://localhost:3001');
 
     analysesPage.waitForPageToLoad();
-    analysesPage.addAnalysis(analysisTitle, 'my outcome', '/example.csv');
+    analysesPage.addAnalysis(analysisTitle, analysisOutcomeTitle, '/example.json');
     browser
       .waitForElementVisible('#analysis-header', 10000)
-      .assert.containsText('dd', analysisTitle);
+      .assert.containsText('#analysisTitle', analysisTitle)
+      .assert.containsText('#analysisOutcome', analysisOutcomeTitle);
     analysesPage.end();
   }
 };
