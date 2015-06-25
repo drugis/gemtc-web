edf00be7202fc5d0bad1a0834aab16477fff4edf
Connor Stroomberg <c.stroomberg@gmail.com>
2015-06-25 17:03:25 +0200

    setup for create model integration test

---
+++ test/integration/addModelToAnalysisTest.js
@@ -0,0 +1,23 @@
+var login = require('./util/login');
+var AnalysesPage = require('./analyses/analysesPage');
+var AnalysisOverviewPage = require('./analyses/analysisOverviewPage');
+
+var analysisTitle = 'my title';
+var analysisOutcomeTitle = 'my outcome';
+
+module.exports = {
+  "gemtc login test": function(browser) {
+    var analysesPage = new AnalysesPage(browser);
+    var analysisOverviewPage = new AnalysisOverviewPage(browser);
+    
+    login(browser, 'http://localhost:3001');
+
+    analysesPage.waitForPageToLoad();
+    analysesPage.addAnalysis(analysisTitle, analysisOutcomeTitle, '/example.json');
+    browser.waitForElementVisible('#analysis-header', 10000);
+
+    analysisOverviewPage.waitForPageToLoad();
+    analysisOverviewPage.addModel();
+    analysisOverviewPage.end();
+  }
+};
