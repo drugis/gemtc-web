6ec72330b00c00eb5b4a802ca190aa389629b762
Connor Stroomberg <c.stroomberg@gmail.com>
2015-06-25 15:38:56 +0200

    evidence table displaying problem details

---
+++ app/js/analyses/evidenceTableServiceSpec.js
@@ -0,0 +1,158 @@
+define(['angular', 'angular-mocks', 'analyses/analyses'], function() {
+  describe('The evidence service', function() {
+
+    var evidenceTableService;
+
+
+    beforeEach(module('gemtc.analyses'));
+
+    beforeEach(inject(function(EvidenceTableService) {
+      evidenceTableService = EvidenceTableService;
+    }));
+
+    describe('studyMapToStudyArray', function() {
+
+      var studyMap = {
+        'Study1': {
+          arms: {
+            'Treatment1': {
+              "responders": 58,
+              "sampleSize": 100
+            },
+            'Treatment2': {
+              "responders": 53,
+              "sampleSize": 103
+            }
+          }
+        },
+        'Study2': {
+          arms: {
+            'Treatment1': {
+              "responders": 58,
+              "sampleSize": 100
+            },
+            'Treatment2': {
+              "responders": 53,
+              "sampleSize": 103
+            }
+          }
+        }
+      };
+      var studyArray;
+
+      beforeEach(inject(function() {
+        studyArray = evidenceTableService.studyMapToStudyArray(studyMap);
+      }));
+
+      it('create a list of study objects from the studyMap', function() {
+        expect(studyArray.length).toEqual(2);
+        expect(studyArray[0].title).toBeDefined();
+        expect(studyArray[0].arms).toBeDefined();
+        expect(studyArray[0].arms.length).toEqual(2);
+        expect(studyArray[0].arms[0].title).toBeDefined();
+        expect(studyArray[0].arms[0].data).toBeDefined();
+      });
+
+    });
+
+    describe('studyListToEvidenceRows', function() {
+
+      var studyList = [{
+        title: 'title1',
+        arms: [{
+          title: 'arm1',
+          data: {
+            responders: 10,
+            sampleSize: 20,
+            mean: 5.5,
+            'std.dev': 0.001
+          }
+        }, {
+          title: 'arm2',
+          data: {}
+        }]
+      }, {
+        title: 'title2',
+        arms: [{
+          title: 'arm3',
+          data: {}
+        }]
+      }];
+      var evidenceRows;
+
+      beforeEach(inject(function() {
+        evidenceRows = evidenceTableService.studyListToEvidenceRows(studyList);
+      }));
+
+      it('create a list of evidenceRows from a list of study objects', function() {
+        expect(evidenceRows.length).toEqual(3);
+        expect(evidenceRows[0].studyTitle).toBeDefined();
+        expect(evidenceRows[0].studyRowSpan).toBeDefined();
+        expect(evidenceRows[0].treatmentTitle).toBeDefined();
+        expect(evidenceRows[0].evidence).toBeDefined();
+        expect(evidenceRows[0].showStudyColumn).toBeDefined();
+
+        expect(evidenceRows[0].evidence.responders).toEqual(10);
+        expect(evidenceRows[0].evidence.sampleSize).toEqual(20);
+        expect(evidenceRows[0].evidence.mean).toEqual(5.5);
+        expect(evidenceRows[0].evidence.stdDev).toEqual(0.001);
+      });
+
+    });
+
+    describe('determineOutcomeType', function() {
+
+      it('should return dichotomous if the first arm in the first study has responders', function() {
+        var studyList = [{
+          title: 'title1',
+          arms: [{
+            title: 'arm1',
+            data: {
+              responders: 10,
+              sampleSize: 20
+            }
+          }, {
+            title: 'arm2',
+            data: {}
+          }]
+        }, {
+          title: 'title2',
+          arms: [{
+            title: 'arm3',
+            data: {}
+          }]
+        }];
+        var outcomeType;
+        outcomeType = evidenceTableService.determineOutcomeType(studyList);
+        expect(outcomeType).toEqual('dichotomous');
+      });
+
+      it('should return continuous if the first arm in the first study has no responders', function() {
+        var studyList = [{
+          title: 'title1',
+          arms: [{
+            title: 'arm1',
+            data: {
+              mean: 10,
+              sampleSize: 20
+            }
+          }, {
+            title: 'arm2',
+            data: {}
+          }]
+        }, {
+          title: 'title2',
+          arms: [{
+            title: 'arm3',
+            data: {}
+          }]
+        }];
+        var outcomeType;
+        outcomeType = evidenceTableService.determineOutcomeType(studyList);
+        expect(outcomeType).toEqual('continuous');
+      });
+
+    });
+
+  });
+});
