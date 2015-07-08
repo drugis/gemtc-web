17b33525e7ce75bb087be3d9b35eeb0a602d66b1
Daan Reid <osmosisch@gmail.com>
2015-07-08 17:05:41 +0200

    Add r-base worker step to minimise rebuilding on R code change; add SVG plots to R output; add SVG plots plus paging to model view

--- app/js/controllers/modelController.js
+++ app/js/controllers/modelController.js
@@ -1,46 +1,21 @@
 'use strict';
 define(['lodash'], function() {
-  var dependencies = ['$scope', '$stateParams', 'ModelResource', 'PataviService',
+  var dependencies = ['$scope', '$sce', '$stateParams', 'ModelResource', 'PataviService',
     'RelativeEffectsTableService', 'PataviTaskIdResource', 'ProblemResource', 'AnalysisResource'
   ];
-  var ModelController = function($scope, $stateParams, ModelResource, PataviService,
+  var ModelController = function($scope, $sce, $stateParams, ModelResource, PataviService,
     RelativeEffectsTableService, PataviTaskIdResource, ProblemResource, AnalysisResource) {
 
     $scope.analysis = AnalysisResource.get($stateParams);
-
-    function getTaskId() {
-      return PataviTaskIdResource.get($stateParams);
-    }
-
     $scope.progress = {
       percentage: 0
     };
-
-    function nameRankProbabilities(rankProbabilities, treatments) {
-      return _.reduce(_.pairs(rankProbabilities), function(memo, pair) {
-        var treatmentName = _.find(treatments, function(treatment) {
-          return treatment.id.toString() === pair[0];
-        }).name;
-        memo[treatmentName] = pair[1];
-        return memo;
-      }, {});
-    }
-
-    function successCallback(result) {
-      return ProblemResource.get({
-        analysisId: $stateParams.analysisId,
-        projectId: $stateParams.projectId
-      }).$promise.then(function(problem) {
-        result.results.rankProbabilities = nameRankProbabilities(result.results.rankProbabilities, problem.treatments);
-        $scope.result = result;
-        var relativeEffects = result.results.relativeEffects;
-        var isLogScale = result.results.logScale;
-        $scope.relativeEffectsTable = RelativeEffectsTableService.buildTable(relativeEffects, isLogScale, problem.treatments);
-      });
-    }
-
+    $scope.showNextSVGPage = showNextSVGPage;
+    $scope.showPreviousSVGPage = showPreviousSVGPage;
     $scope.model = ModelResource.get($stateParams);
     $scope.$parent.model = $scope.model;
+    $scope.selectedPage = 0;
+
     $scope.model
       .$promise
       .then(getTaskId)
@@ -54,6 +29,53 @@ define(['lodash'], function() {
             $scope.progress.percentage = update.progress;
           }
         });
+
+    function showNextSVGPage() {
+      ++$scope.selectedPage;
+    }
+
+    function showPreviousSVGPage() {
+      --$scope.selectedPage;
+    }
+
+    function getTaskId() {
+      return PataviTaskIdResource.get($stateParams);
+    }
+
+    function nameRankProbabilities(rankProbabilities, treatments) {
+      return _.reduce(_.pairs(rankProbabilities), function(memo, pair) {
+        var treatmentName = _.find(treatments, function(treatment) {
+          return treatment.id.toString() === pair[0];
+        }).name;
+        memo[treatmentName] = pair[1];
+        return memo;
+      }, {});
+    }
+
+    function trustRelativeEffectPlots(treatments) {
+      var trustedTreatments = {};
+      _.forEach(treatments, function(treatmentPlots, treatmentId) {
+        trustedTreatments[treatmentId] = _.map(treatmentPlots, function(plot) {
+          return $sce.trustAsHtml(plot);
+        });
+      });
+      return trustedTreatments;
+    }
+
+    function successCallback(result) {
+      return ProblemResource.get({
+        analysisId: $stateParams.analysisId,
+        projectId: $stateParams.projectId
+      }).$promise.then(function(problem) {
+        result.results.rankProbabilities = nameRankProbabilities(result.results.rankProbabilities, problem.treatments);
+        result.results.relativeEffectPlots = trustRelativeEffectPlots(result.results.relativeEffectPlots);
+        $scope.result = result;
+        var relativeEffects = result.results.relativeEffects;
+        var isLogScale = result.results.logScale;
+        $scope.relativeEffectsTable = RelativeEffectsTableService.buildTable(relativeEffects, isLogScale, problem.treatments);
+      });
+    }
+
 
 
   };
