824752b67b4f7d8de53160a8a9d3d51a721951b8
Daan Reid <osmosisch@gmail.com>
2015-07-10 15:52:00 +0200

    Batch model creation

--- app/js/models/createModelController.js
+++ app/js/models/createModelController.js
@@ -1,8 +1,9 @@
 'use strict';
-define([], function() {
-  var dependencies = ['$scope', '$stateParams', '$location', 'AnalysisResource', 'ModelResource', 'AnalysisService', 'ProblemResource'];
-  var CreateModelController = function($scope, $stateParams, $location, AnalysisResource, ModelResource, AnalysisService, ProblemResource) {
-
+define(['lodash'], function(_) {
+  var dependencies = ['$scope', '$q', '$stateParams', '$state', '$location',
+    'AnalysisResource', 'ModelResource', 'AnalysisService', 'ProblemResource'];
+  var CreateModelController = function($scope, $q, $stateParams, $state, $location,
+    AnalysisResource, ModelResource, AnalysisService, ProblemResource) {
 
     var problemDefer = ProblemResource.get($stateParams);
 
@@ -26,8 +27,40 @@ define([], function() {
         !!$scope.isAddingModel;
     }
 
+    function createModelBatch(modelBase) {
+      return _.map($scope.comparisonOptions, function(comparisonOption) {
+        return {
+          title: modelBase.title + ' (' + comparisonOption.from.name + ' - ' + comparisonOption.to.name + ')',
+          linearModel: modelBase.linearModel,
+          modelType: {
+            type: 'pairwise'
+          },
+          pairwiseComparison: comparisonOption
+        };
+      });
+    }
+
     function createModel(model) {
       $scope.isAddingModel = true;
+      if (model.modelType.type === 'all-pairwise') {
+        var modelsToCreate = createModelBatch(model);
+        var creationPromises = _.map(modelsToCreate, function(modelToCreate) {
+          return createAndPostModel(modelToCreate, function() {});
+        });
+        $q.all(creationPromises).then(function() {
+          $scope.isAddingModel = false;
+          $state.go('analysis', $stateParams);
+        });
+      } else {
+        createAndPostModel(model, function(result, headers) {
+          $scope.isAddingModel = false;
+          // Call to replace is needed to have backbutton skip the createModel view when going back from the model View
+          $location.url(headers().location).replace();
+        });
+      }
+    }
+
+    function createAndPostModel(model, successFunction) {
       if (model.modelType.type === 'pairwise') {
         model.modelType.details = {
           from: model.pairwiseComparison.from.name,
@@ -35,13 +68,10 @@ define([], function() {
         };
       }
       var pureModel = _.omit(model, 'pairwiseComparison');
-      ModelResource.save($stateParams, pureModel, function(result, headers) {
-        $scope.isAddingAnalysis = false;
-        // Call to replace is needed to have backbutton skip the createModel view when going back from the model View
-        $location.url(headers().location).replace();
-      });
-    };
+      return ModelResource.save($stateParams, pureModel, successFunction).$promise;
 
-  }
+    }
+  };
+
   return dependencies.concat(CreateModelController);
 });
