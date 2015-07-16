bbba5f80c4a87edb106db615aa4d2dc0bb083c02
Daan Reid <osmosisch@gmail.com>
2015-07-16 08:38:00 +0200

    change routing to comply with addis core

--- app/js/models/modelsController.js
+++ app/js/models/modelsController.js
@@ -1,10 +1,12 @@
 'use strict';
-define([], function() {
+define(['lodash'], function(_) {
   var dependencies = ['$scope', '$state', '$stateParams', 'ModelResource'];
   var ModelsController = function($scope, $state, $stateParams, ModelResource) {
     $scope.modelsLoaded = false;
     $scope.analysisId = $stateParams.analysisId;
-
+    $scope.modelParams = function(model) {
+      return _.extend($stateParams, {modelId: model.id});
+    }
     $scope.loadModels = function() {
       $scope.models = ModelResource.query($stateParams, function(result) {
         $scope.modelsLoaded = true;
