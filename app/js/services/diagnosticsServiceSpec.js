c4c6146e09e9e127e86760aa352aec2c0739759e
Connor Stroomberg <c.stroomberg@gmail.com>
2015-08-12 16:40:11 +0200

    sort convergence diagnostics

--- app/js/services/diagnosticsServiceSpec.js
+++ app/js/services/diagnosticsServiceSpec.js
@@ -55,34 +55,32 @@ define(['angular', 'angular-mocks', 'services'], function() {
           }
         };
 
-        var expected = {
-          'd.2.3': {
-            label: 'd.2.3 (Fluoxetine, Paroxetine)',
-            'Point est.': 1.0004,
-            'Upper C.I.': 1.0011
-          },
-          'd.2.4': {
-            label: 'd.2.4 (Fluoxetine, Venlafaxine)',
-            'Point est.': 1.0005,
-            'Upper C.I.': 1.0017
-          },
-          'd.2.5': {
-            label: 'd.2.5 (Fluoxetine, Sertraline)',
-            'Point est.': 1.0002,
-            'Upper C.I.': 1.0005
-          },
-          'sd.d': {
-            label: 'sd.d (Random effects standard deviation)',
-            'Point est.': 1.0007,
-            'Upper C.I.': 1.0011
-          },
-          'd.indirect': {
-            label: 'd.indirect (Indirect estimate)',
-            'Point est.': 1.0008,
-            'Upper C.I.': 1.0012
-          }
-        };
-
+        var expected = [{
+          key: 'd.2.3',
+          label: 'd.2.3 (Fluoxetine, Paroxetine)',
+          'Point est.': 1.0004,
+          'Upper C.I.': 1.0011
+        }, {
+          key: 'd.2.4',
+          label: 'd.2.4 (Fluoxetine, Venlafaxine)',
+          'Point est.': 1.0005,
+          'Upper C.I.': 1.0017
+        }, {
+          key: 'd.2.5',
+          label: 'd.2.5 (Fluoxetine, Sertraline)',
+          'Point est.': 1.0002,
+          'Upper C.I.': 1.0005
+        }, {
+          key: 'sd.d',
+          label: 'sd.d (Random effects standard deviation)',
+          'Point est.': 1.0007,
+          'Upper C.I.': 1.0011
+        }, {
+          key: 'd.indirect',
+          label: 'd.indirect (Indirect estimate)',
+          'Point est.': 1.0008,
+          'Upper C.I.': 1.0012
+        }];
         var labeledDiagnostics = diagnosticsService.labelDiagnostics(gelmanDiagnostics, treatments);
         expect(labeledDiagnostics).toEqual(expected);
 
