2b0b85a8d7e7171a8bb76ca1bed025096369455f
Connor Stroomberg <c.stroomberg@gmail.com>
2015-08-21 11:09:56 +0200

    inline alert directive template

--- app/js/util/appAlertDirective.js
+++ app/js/util/appAlertDirective.js
@@ -11,14 +11,25 @@ define([], function() {
         close: '&',
         error: '='
       },
-      templateUrl: 'js/util/appAlertDirective.html',
       link: function(scope, element) {
         scope.animatedClose = function() {
           $(element).fadeOut(200, function() {
             scope.close();
           });
         };
-      }
+      }, // use a inline template for easy use in addis
+      template: '<div class="alert-box {{type}}"> ' +
+                '<div class="alert-box-message"> ' +
+                  '<a ng-click="animatedClose()" class="close" style="top: 1rem;">&times;</a>  ' +
+                  '<div ng-if="error.type !== &quot;patavi&quot;"> ' +
+                    '{{error.code}} {{error.cause}} {{error.message}} ' +
+                  '</div> ' +
+                  '<div ng-if="error.type === &quot;patavi&quot;"> ' +
+                    '<p>An error has occured while running the model in R.</p> ' +
+                    '{{error.message}} ' +
+                  '</div> ' +
+                '</div> ' +
+              '</div> '
     };
   };
   return dependencies.concat(AppAlertDirective);
