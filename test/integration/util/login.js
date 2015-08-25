f01b2dff6581ca65d392480bc07b70c47f56bb06
Connor Stroomberg <c.stroomberg@gmail.com>
2015-08-25 16:57:40 +0200

    alway try to logon

--- test/integration/util/login.js
+++ test/integration/util/login.js
@@ -3,16 +3,16 @@ module.exports = function(browser, url) {
         .url(url)
         .waitForElementVisible('button[type="submit"]', 5000)
         .click('button[type="submit"]')
-        .pause(1000);
+        .pause(1000)
+        .assert.containsText('h2', 'Sign in with your Google Account')
+        .pause(1000)
+        .setValue('input[type=email]', 'addistestuser1@gmail.com')
+        .click('input[type="submit"]')
+        .pause(1000)
+        .setValue('input[type=password]', 'speciaalvoordejenkins')
+        .click('#signIn')
     if (process.env.GEMTC_NIGHTWATCH_URL === 'http://localhost:3001') {
-        browser.assert.containsText('h2', 'Sign in with your Google Account')
-            .pause(1000)
-            .setValue('input[type=email]', 'addistestuser1@gmail.com')
-            .click('input[type="submit"]')
-            .pause(1000)
-            .setValue('input[type=password]', 'speciaalvoordejenkins')
-            .click('#signIn')
-            .pause(3000) // wait for submit button to become active (thanks for keeping us safe google)
+        browser.pause(3000) // wait for submit button to become active (thanks for keeping us safe google)
             .click('#submit_approve_access')
     }
 
