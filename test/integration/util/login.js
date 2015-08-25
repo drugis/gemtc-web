5deab8401cd1062de08fff80f05996c228c46be8
Connor Stroomberg <c.stroomberg@gmail.com>
2015-08-25 16:50:36 +0200

    fix sysntax error

--- test/integration/util/login.js
+++ test/integration/util/login.js
@@ -1,20 +1,20 @@
 module.exports = function(browser, url) {
-  browser
-    .url(url)
-    .waitForElementVisible('button[type="submit"]', 5000)
-    .click('button[type="submit"]')
-    .pause(1000);
+    browser
+        .url(url)
+        .waitForElementVisible('button[type="submit"]', 5000)
+        .click('button[type="submit"]')
+        .pause(1000);
     if (process.env.GEMTC_NIGHTWATCH_URL === 'http://localhost:3001') {
-    .assert.containsText('h2', 'Sign in with your Google Account')
-    .pause(1000)
-    .setValue('input[type=email]', 'addistestuser1@gmail.com')
-    .click('input[type="submit"]')
-    .pause(1000)
-    .setValue('input[type=password]', 'speciaalvoordejenkins')
-    .click('#signIn')
-    .pause(3000); // wait for submit button to become active (thanks for keeping us safe google)
-    .browser.click('#submit_approve_access')
-    } 
+        browser.assert.containsText('h2', 'Sign in with your Google Account')
+            .pause(1000)
+            .setValue('input[type=email]', 'addistestuser1@gmail.com')
+            .click('input[type="submit"]')
+            .pause(1000)
+            .setValue('input[type=password]', 'speciaalvoordejenkins')
+            .click('#signIn')
+            .pause(3000) // wait for submit button to become active (thanks for keeping us safe google)
+            .click('#submit_approve_access')
+    }
 
-  return browser;
+    return browser;
 };
