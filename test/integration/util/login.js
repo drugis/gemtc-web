8d1b462918866a1cb947ce4d6e79d77c692cea1d
Connor Stroomberg <c.stroomberg@gmail.com>
2015-08-25 15:49:04 +0200

    test with check for localhost

--- test/integration/util/login.js
+++ test/integration/util/login.js
@@ -1,26 +1,20 @@
 module.exports = function(browser, url) {
   browser
     .url(url)
-
     .waitForElementVisible('button[type="submit"]', 50000)
     .click('button[type="submit"]')
-        .source(function (result){
-        // Source will be stored in result.value
-        console.log(result.value);
-    })
-    .waitForElementVisible('body', 50000)
-    .assert.containsText('h2', 'Sign in with your Google Account')
-    .pause(1000)
-    .setValue('input[type=email]', 'addistestuser1@gmail.com')
-    .click('input[type="submit"]')
-    .pause(1000)
-    .setValue('input[type=password]', 'speciaalvoordejenkins')
-    .click('#signIn')
-    .pause(3000); // wait for submit button to become active (thanks for keeping us safe google)
-
-    if (process.env.GEMTC_NIGHTWATCH_URL === 'http://localhost:3001') {
-      browser.click('#submit_approve_access')
-    } 
+  if (process.env.GEMTC_NIGHTWATCH_URL === 'http://localhost:3001') {
+    browser.waitForElementVisible('body', 50000)
+      .assert.containsText('h2', 'Sign in with your Google Account')
+      .pause(1000)
+      .setValue('input[type=email]', 'addistestuser1@gmail.com')
+      .click('input[type="submit"]')
+      .pause(1000)
+      .setValue('input[type=password]', 'speciaalvoordejenkins')
+      .click('#signIn')
+      .pause(3000) // wait for submit button to become active (thanks for keeping us safe google)
+    .click('#submit_approve_access')
+  }
 
   return browser;
 };
