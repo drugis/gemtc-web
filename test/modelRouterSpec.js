749afbb37ce0f0389aa0c77657dd844b3e8b0af1
Daan Reid <osmosisch@gmail.com>
2015-10-07 10:51:30 +0200

    clean up server after test; use deep equality in body comparison

--- test/modelRouterSpec.js
+++ test/modelRouterSpec.js
@@ -16,48 +16,47 @@ var modelRouter = proxyquire('../standalone-app/modelRouter', {
   './pataviTaskRepository': pataviTaskRepository
 });
 
-
 describe('modelRouter', function() {
   var server;
   before(function() {
     server = app.use('/analyses/:analysisId/models', modelRouter).listen(3999);
   });
-
   after(function() {
     server.close();
   });
 
-  describe('GET request to /', function() {
+  describe('request to /', function() {
     var taskId = 101;
     var taskId2 = 102;
+    beforeEach(function() {
 
-    var models = [{
-      id: 1,
-      title: 'model1'
-    }, {
-      id: 2,
-      title: 'model2',
-      taskId: taskId
-    }, {
-      id: 3,
-      title: 'model3',
-      taskId: taskId2
-    }];
+      var models = [{
+        id: 1,
+        title: 'model1'
+      }, {
+        id: 2,
+        title: 'model2',
+        taskId: taskId
+      }, {
+        id: 3,
+        title: 'model3',
+        taskId: taskId2
+      }];
 
-    var pataviTasks = [{
-      id: taskId,
-      hasresult: true
-    }, {
-      id: taskId2,
-      hasresult: false
-    }];
+      var pataviTasks = [{
+        id: taskId,
+        hasresult: true
+      }, {
+        id: taskId2,
+        hasresult: false
+      }];
 
-    sinon.stub(modelRepository, "findByAnalysis").onCall(0).yields(null, models);
-    sinon.stub(pataviTaskRepository, "getPataviTasksStatus").onCall(0).yields(null, pataviTasks);
+      sinon.stub(modelRepository, "findByAnalysis").onCall(0).yields(null, models);
+      sinon.stub(pataviTaskRepository, "getPataviTasksStatus").onCall(0).yields(null, pataviTasks);
+    });
 
-    it('should find all models', function(done) {
-
-      var expextedResult = [{
+    it('find all models', function(done) {
+      var expectedResult = [{
         id: 2,
         title: 'model2',
         taskId: taskId,
@@ -72,47 +71,7 @@ describe('modelRouter', function() {
       request('GET', 'http://localhost:3999/analyses/1/models/')
         .end(function(err, res) {
           res.should.have.property('status', 200);
-          res.should.have.property('body', expextedResult);
-          done();
-        });
-
-    });
-  });
-
-  describe('POST request to /', function() {
-    it('should create a model', function(done) {
-      var newModel = {};
-      request('POST', 'http://localhost/3999/analyses/1/models/', newModel)
-        .end(function(err, res) {
-          assert(undefined, err);
-          res.should.have.property('status', 201)
-          done();
-        });
-
-    });
-  });
-
-  describe('POST request to /something', function(done) {
-    request('POST', 'http://localhost/3999/analyses/1/models/2/something', {})
-      .end(function(err, res) {
-        assert(undefined, err);
-        res.should.have.property('status', 201)
-        done();
-      });
-  });
-
-  describe('POST request to /1/extendRunLength', function() {
-
-    it('should check the ownership', function(done) {
-      var runLengthSettings = {
-        burnInIterations: 3000,
-        inferenceIterations: 20000,
-        thinningFactor: 10
-      };
-      request('POST', 'http://localhost/3999/analyses/1/models/1/extendRunLength', runLengthSettings)
-        .end(function(err, res) {
-          assert(undefined, err);
-          res.should.have.property('status', 200)
+          assert.deepEqual(expectedResult[0], res.body[0])
           done();
         });
     });
