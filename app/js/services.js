7c4f92c2897e314fde896921c076c80db84dd438
Connor Stroomberg <c.stroomberg@gmail.com>
2015-10-09 12:14:41 +0200

    expose modules for use in addis

--- app/js/services.js
+++ app/js/services.js
@@ -4,7 +4,7 @@ define(function(require) {
   return angular.module('gemtc.services', [])
     .factory('PataviService', require('gemtc-web/services/pataviService'))
     .factory('RelativeEffectsTableService', require('gemtc-web/services/relativeEffectsTableService'))
-    .factory('DevianceStatisticsService', require('models/devianceStatisticsService'))
+    .factory('DevianceStatisticsService', require('gemtc-web/models/devianceStatisticsService'))
 
     .factory('NetworkPlotService', require('gemtc-web/analyses/networkPlotService'))
     .factory('AnalysisService', require('gemtc-web/analyses/analysisService'))
