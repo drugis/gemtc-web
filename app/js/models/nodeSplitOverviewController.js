6c78ecd90368e458dc75c3bbb9106c9bdf4b4eeb
Connor Stroomberg <c.stroomberg@gmail.com>
2015-10-14 19:44:39 +0200

    WIP fill out view

--- app/js/models/nodeSplitOverviewController.js
+++ app/js/models/nodeSplitOverviewController.js
@@ -3,29 +3,78 @@ define(['angular', 'lodash'], function(angular, _) {
   var dependencies = ['$scope', '$q', '$stateParams', '$state', 'models', 'problem', 'AnalysisService', 'ModelResource'];
   var NodeSplitOverviewController = function($scope, $q, $stateParams, $state, models, problem, AnalysisService, ModelResource) {
 
-    $scope.models = models;
     $scope.goToModel = goToModel;
-    $scope.comparisons = _.map(AnalysisService.createNodeSplitOptions(problem), buildComparison);
+    $scope.analysis.$promise.then(buildComparisons);
 
-    function buildComparison(comparison) {
-      var model = findModelForComparison(comparison, $scope.models);
+    $scope.model.$promise.then(function() {
+      if ($scope.model.modelType.type === 'node-split') {
+        var networkModel = findNetworkModelForModel($scope.model, models);
+        if (networkModel) {
+          var getParams = $stateParams;
+          getParams.modelId = networkModel.id
+          $scope.networkModel = ModelResource.get(getParams);
+        }
+      } else {
+        $scope.networkModel = $scope.model;
+      }
+      $scope.networkModel.$promise.then(function(networkModel) {
+        if (networkModel.taskId) {
+          $scope.networkModel.result = getModelResult(networkModel.id);
+        }
+      });
+    });
+
+    function buildComparisons(analysis) {
+      $scope.comparisons = _.map(AnalysisService.createNodeSplitOptions(analysis.problem), function(comparison) {
+        var model = findModelForComparison(comparison, models);
+        if (model) {
+          return buildComparisonTableRow(comparison, model);
+        } else {
+          return comparison;
+        }
+      });
+    }
+
+    function findNetworkModelForModel(nodeSplitModel, models) {
+      return _.find(models, function(model) {
+        return model.modelType.type === 'network' && isSameModelSettings(nodeSplitModel, model);
+      })
+    }
+
+    function isSameModelSettings(model1, model2) {
+      return (
+        model1.likelihood === model2.likelihood &&
+        model1.link === model2.link &&
+        model1.linearModel === model2.linearModel &&
+        model1.outcomeScale.type === model2.outcomeScale.type &&
+        model1.outcomeScale.value === model2.outcomeScale.value
+      );
+    }
+
+    function buildComparisonTableRow(comparison, model) {
+
       var modelResult;
+
       if (model && model.taskId) {
-        modelResult = ModelResource.getResult({
-          analysisId: $stateParams.analysisId,
-          modelId: model.id
-        });
+        modelResult = getModelResult(model.id);
       }
 
-      return model ? {
+      return {
         modelTitle: model.title,
         label: comparison.label,
         result: modelResult
-      } : comparison;
+      }
+    }
+
+    function getModelResult(modelId) {
+      var getParams = $stateParams;
+      getParams.modelId = modelId;
+      return ModelResource.getResult(getParams);
     }
 
     function findModelForComparison(comparison, models) {
       return _.find(models, function(model) {
+        // todo: model settings should be equal.
         return (
           model.modelType.type === 'node-split' &&
           model.modelType.details.from.id === comparison.from.id &&
