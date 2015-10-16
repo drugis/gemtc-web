1e513d10bc0208317fcc4d94e220f6e4b8c6a5ac
Connor Stroomberg <c.stroomberg@gmail.com>
2015-10-16 12:07:26 +0200

    Show estimates in nodesplit overview

--- app/js/models/nodeSplitOverviewController.js
+++ app/js/models/nodeSplitOverviewController.js
@@ -1,11 +1,12 @@
 'use strict';
 define(['angular', 'lodash'], function(angular, _) {
-  var dependencies = ['$scope', '$stateParams', '$state', '$modal', 'gemtcRootPath', 'models', 'problem', 'AnalysisService', 'ModelResource'];
-  var NodeSplitOverviewController = function($scope, $stateParams, $state, $modal, gemtcRootPath, models, problem, AnalysisService, ModelResource) {
+  var dependencies = ['$scope', '$q', '$stateParams', '$state', '$modal', 'gemtcRootPath', 'models', 'problem', 'AnalysisService', 'ModelResource', 'NodeSplitOverviewService'];
+  var NodeSplitOverviewController = function($scope, $q, $stateParams, $state, $modal, gemtcRootPath, models, problem, AnalysisService, ModelResource, NodeSplitOverviewService) {
 
     $scope.goToModel = goToModel;
-    $scope.analysis.$promise.then(buildComparisons);
     $scope.openCreateNodeSplitDialog = openCreateNodeSplitDialog;
+    $scope.networkModelResultsDefer = $q.defer();
+    $scope.analysis.$promise.then(buildComparisons);
 
     $scope.model.$promise.then(function() {
       if ($scope.model.modelType.type === 'node-split') {
@@ -18,12 +19,99 @@ define(['angular', 'lodash'], function(angular, _) {
       } else {
         $scope.networkModel = $scope.model;
       }
-      $scope.networkModel.$promise.then(function(networkModel) {
-        if (networkModel.taskId) {
-          $scope.networkModel.result = getModelResult(networkModel.id);
-        }
-      });
+
+      if ($scope.networkModel) {
+        $scope.networkModel.$promise.then(function(networkModel) {
+          if (networkModel.taskId) {
+            $scope.networkModel.result = getModelResult(networkModel.id);
+            $scope.networkModel.result.$promise.then($scope.networkModelResultsDefer.resolve);
+          }
+        });
+      }
     });
+
+    function buildComparisons(analysis) {
+      $scope.comparisons = _.map(AnalysisService.createNodeSplitOptions(analysis.problem), function(comparison) {
+        var row = comparison;
+        var model = findModelForComparison(comparison, models);
+
+        if (model) {
+          row = buildComparisonTableRow(comparison, model);
+        }
+
+        $scope.networkModelResultsDefer.promise.then(function(result) {
+          fillConsistencyCell(row, result);
+        });
+
+        return row;
+      });
+    }
+
+    function findNetworkModelForModel(nodeSplitModel, models) {
+      return _.find(models, function(model) {
+        return model.modelType.type === 'network' && isSameModelSettings(nodeSplitModel, model);
+      })
+    }
+
+    function isSameModelSettings(model1, model2) {
+      return (
+        model1.likelihood === model2.likelihood &&
+        model1.link === model2.link &&
+        model1.linearModel === model2.linearModel &&
+        ((model1.outcomeScale === undefined && model2.outcomeScale === undefined) ||
+          (model1.outcomeScale.type === model2.outcomeScale.type &&
+            model1.outcomeScale.value === model2.outcomeScale.value)
+        )
+      );
+    }
+
+    function buildComparisonTableRow(comparison, model) {
+      var row = {
+        label: comparison.label,
+        from: comparison.from,
+        to: comparison.to,
+        modelTitle: model.title,
+        modelId: model.id,
+        hasModel: true
+      };
+
+      if (model.taskId) {
+        row.result = getModelResult(model.id);
+        row.result.$promise.then(function(result) {
+          row.directEffectEstimate = NodeSplitOverviewService.buildDirectEffectEstimates(result);
+          row.inDirectEffectEstimate = NodeSplitOverviewService.buildIndirectEffectEstimates(result);
+        });
+      }
+
+      return row;
+    }
+
+    function fillConsistencyCell(row, result) {
+      row.consistencyEstimate = NodeSplitOverviewService.buildConsistencyEstimates(result, row);
+    }
+
+    function getModelResult(modelId) {
+      var getParams = $stateParams;
+      getParams.modelId = modelId;
+      return ModelResource.getResult(getParams);
+    }
+
+    function findModelForComparison(comparison, models) {
+      return _.find(models, function(model) {
+        // todo: model settings should be equal.
+        return (
+          model.modelType.type === 'node-split' &&
+          model.modelType.details.from.id === comparison.from.id &&
+          model.modelType.details.to.id === comparison.to.id
+        );
+      });
+    }
+
+    function goToModel(modelId) {
+      var goToModelParams = angular.copy($stateParams);
+      goToModelParams.modelId = modelId;
+      $state.go('model', goToModelParams);
+    }
 
     function openCreateNodeSplitDialog(comparison) {
       $modal.open({
@@ -52,76 +140,6 @@ define(['angular', 'lodash'], function(angular, _) {
         }
       });
     };
-
-
-    function buildComparisons(analysis) {
-      $scope.comparisons = _.map(AnalysisService.createNodeSplitOptions(analysis.problem), function(comparison) {
-        var model = findModelForComparison(comparison, models);
-        if (model) {
-          return buildComparisonTableRow(comparison, model);
-        } else {
-          return comparison;
-        }
-      });
-    }
-
-    function findNetworkModelForModel(nodeSplitModel, models) {
-      return _.find(models, function(model) {
-        return model.modelType.type === 'network' && isSameModelSettings(nodeSplitModel, model);
-      })
-    }
-
-    function isSameModelSettings(model1, model2) {
-      return (
-        model1.likelihood === model2.likelihood &&
-        model1.link === model2.link &&
-        model1.linearModel === model2.linearModel &&
-        ( (model1.outcomeScale === undefined && model2.outcomeScale === undefined) ||
-          (model1.outcomeScale.type === model2.outcomeScale.type &&
-            model1.outcomeScale.value === model2.outcomeScale.value)
-        )
-      );
-    }
-
-    function buildComparisonTableRow(comparison, model) {
-
-      var modelResult;
-
-      if (model && model.taskId) {
-        modelResult = getModelResult(model.id);
-      }
-
-      return {
-        modelTitle: model.title,
-        modelId: model.id,
-        hasModel: true,
-        label: comparison.label,
-        result: modelResult
-      }
-    }
-
-    function getModelResult(modelId) {
-      var getParams = $stateParams;
-      getParams.modelId = modelId;
-      return ModelResource.getResult(getParams);
-    }
-
-    function findModelForComparison(comparison, models) {
-      return _.find(models, function(model) {
-        // todo: model settings should be equal.
-        return (
-          model.modelType.type === 'node-split' &&
-          model.modelType.details.from.id === comparison.from.id &&
-          model.modelType.details.to.id === comparison.to.id
-        );
-      });
-    }
-
-    function goToModel(modelId) {
-      var goToModelParams = angular.copy($stateParams);
-      goToModelParams.modelId = modelId;
-      $state.go('model', goToModelParams);
-    }
 
 
   };
---
+++ app/js/models/nodeSplitOverviewService.js
@@ -0,0 +1,46 @@
+'use strict';
+define(['angular', 'lodash'], function(angular, _) {
+  var dependencies = [];
+
+  var NodeSplitOverviewService = function() {
+
+    function buildDirectEffectEstimates(result) {
+      var quantiles = result.summaries.quantiles;
+      return {
+        mean: quantiles['d.direct']['50%'],
+        lower: quantiles['d.direct']['2.5%'],
+        upper: quantiles['d.direct']['97.5%']
+      };
+    }
+
+
+    function buildIndirectEffectEstimates(result) {
+      var quantiles = result.summaries.quantiles;
+      return {
+        mean: quantiles['d.indirect']['50%'],
+        lower: quantiles['d.indirect']['2.5%'],
+        upper: quantiles['d.indirect']['97.5%']
+      };
+    }
+
+    function buildConsistencyEstimates(networkResult, comparison) {
+      var relativeEffectCosistancyEstimate = _.find(networkResult.relativeEffects, function(relativeEffect) {
+        return comparison.from.id === parseInt(relativeEffect.t1) &&
+          comparison.to.id === parseInt(relativeEffect.t2);
+      });
+
+      return {
+        mean: relativeEffectCosistancyEstimate.quantiles['50%'],
+        lower: relativeEffectCosistancyEstimate.quantiles['2.5%'],
+        upper: relativeEffectCosistancyEstimate.quantiles['97.5%']
+      };
+    }
+
+    return {
+      buildDirectEffectEstimates: buildDirectEffectEstimates,
+      buildIndirectEffectEstimates: buildIndirectEffectEstimates,
+      buildConsistencyEstimates: buildConsistencyEstimates
+    };
+  };
+  return dependencies.concat(NodeSplitOverviewService);
+});
