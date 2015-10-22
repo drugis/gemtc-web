97bb3cfd348e4a3ac33c89044db686854603d373
Daan Reid <osmosisch@gmail.com>
2015-10-22 12:52:04 +0200

    Make directive usable externally

--- app/js/directives.js
+++ app/js/directives.js
@@ -10,5 +10,6 @@ define(function(require) {
     .directive('appAlert', require('gemtc-web/util/appAlertDirective'))
     .directive('runLength', require('gemtc-web/models/runLength/runLengthDirective'))
     .directive('nodesplitForestPlot',require('gemtc-web/models/nodesplitForestPlot/nodesplitForestPlotDirective'))
+    .directive('heterogeneityPrior', require('gemtc-web/models/heterogeneityPrior/heterogeneityPriorDirective'))
   ;
 });
