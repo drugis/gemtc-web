277d003942e5b9b72bd0fb2eee518c807c677b48
Connor Stroomberg <c.stroomberg@gmail.com>
2015-10-30 17:02:42 +0100

    tooltip setup + test for analyses

--- app/js/main.js
+++ app/js/main.js
@@ -14,10 +14,11 @@ require.config({
     'd3': 'bower_components/d3/d3.min',
     'nvd3': 'bower_components/nvd3/build/nv.d3.min',
     'moment': 'bower_components/moment/min/moment.min',
-    'mmfoundation': 'bower_components/angular-foundation/mm-foundation-tpls',
+    'mmfoundation': 'bower_components/angular-foundation/dist/mm-foundation-tpls-0.9.0-SNAPSHOT',
     'angularanimate': 'bower_components/angular-animate/angular-animate.min',
     'ngSanitize': 'bower_components/angular-sanitize/angular-sanitize.min',
-    'papaparse': 'bower_components/papaparse/papaparse.min'
+    'papaparse': 'bower_components/papaparse/papaparse.min',
+    'help-popup': 'bower_components/help-popup/help-directive'
   },
   shim: {
     'jQuery': {
@@ -31,6 +32,9 @@ require.config({
       deps: ['angular']
     },
     'mmfoundation': {
+      deps: ['angular']
+    },
+    'help-popup': {
       deps: ['angular']
     },
     'angular-resource': {
