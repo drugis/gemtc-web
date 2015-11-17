0b35f7e26d61d31eef93de707e9567c47229dea6
Connor Stroomberg <c.stroomberg@gmail.com>
2015-11-17 16:40:00 +0100

    filter direct and indirect row from diagnostics table when model type is node-split

--- app/js/services/diagnosticsService.js
+++ app/js/services/diagnosticsService.js
@@ -3,13 +3,19 @@ define(['lodash'], function(_) {
   var dependencies = [];
 
   var DiagnosticsService = function() {
+
+
+    function skipRowForNodeSplit(modelType, key) {
+      // skip the direct and indirect rows when dealing with a nodesplit model`
+      return modelType === 'node-split' && (key === 'd.direct' || key === 'd.indirect');
+    }
     /**
      * Extend gelman diagnostics with a human-readable label
      * look up treatment names by id.
      * example: d.2.3 would get the label 'd.2.3 (fluox - parox)'
      * if the treatments were [{id: 2, name: fluox}, {id: 3, name: parox}]
      **/
-    function labelDiagnostics(diagnostics, treatments) {
+    function labelDiagnostics(modelType, diagnostics, treatments) {
       function createLabel(diagnostic, key) {
         if (key === 'sd.d') {
           return 'sd.d (Random effects standard deviation)';
@@ -30,11 +36,13 @@ define(['lodash'], function(_) {
       var treatmentsById = _.indexBy(treatments, 'id');
       var diagnosticsWithoutDeviance = _.omit(diagnostics, 'deviance');
       return _.reduce(diagnosticsWithoutDeviance, function(accum, diagnostic, key) {
-        var diagnosticsWithLabel = _.extend(diagnostic, {
-          label: createLabel(diagnostic, key),
-          key: key
-        });
-        accum.push(diagnosticsWithLabel);
+        if (!skipRowForNodeSplit(modelType, key)) {
+          var diagnosticsWithLabel = _.extend(diagnostic, {
+            label: createLabel(diagnostic, key),
+            key: key
+          });
+          accum.push(diagnosticsWithLabel);
+        }
         return accum;
       }, []);
     }
