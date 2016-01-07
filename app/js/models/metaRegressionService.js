9912e3acbb104b2abcd91fc7c0fa81570b6f6efb
Connor Stroomberg <c.stroomberg@gmail.com>
2016-01-07 12:27:50 +0100

    add metaRegressionService and view to plots, forgot to add in previous commit

---
+++ app/js/models/metaRegressionService.js
@@ -0,0 +1,34 @@
+'use strict';
+define(['lodash'], function(_) {
+  var dependencies = [];
+
+  var MetaRegressionService = function() {
+
+    /*
+     ** Transform covariate effects plots object to a list of objects,
+     ** each object has treatmentName and a plot for the corresponding treatment
+     ** 
+     ** The returned list is sorted by treatmentName
+     */
+    function buildCovariatePlotOptions(result, problem) {
+      var treatmentsById = _.indexBy(problem.treatments, 'id');
+      return Object
+        .keys(result.results.covariateEffectPlot)
+        .map(function(key) {
+          return {
+            treatmentName: treatmentsById[key].name,
+            plot: result.results.covariateEffectPlot[key]
+          };
+        })
+        .sort(function(a, b) {
+          return a.treatmentName.localeCompare(b.treatmentName);
+        });
+    }
+
+    return {
+      buildCovariatePlotOptions: buildCovariatePlotOptions
+    };
+  };
+
+  return dependencies.concat(MetaRegressionService);
+});
