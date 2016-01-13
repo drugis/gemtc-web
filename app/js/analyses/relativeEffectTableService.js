1b2aad8eb04d3c7f273bd8f9c74129758222331c
Daan Reid <osmosisch@gmail.com>
2016-01-13 17:03:20 +0100

    Scaffold for relative effects table

---
+++ app/js/analyses/relativeEffectTableService.js
@@ -0,0 +1,12 @@
+'use strict';
+define(['lodash'], function(_) {
+  var dependencies = [];
+
+  var RelativeEffectTableService = function() {
+    return {
+    };
+
+  };
+
+  return dependencies.concat(RelativeEffectTableService);
+});
