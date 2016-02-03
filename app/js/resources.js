55ec28c056413c41bc2cc2c9e61603d48aaad0eb
Connor Stroomberg <c.stroomberg@gmail.com>
2016-02-03 15:22:55 +0100

    bug fix , typo

--- app/js/resources.js
+++ app/js/resources.js
@@ -1,6 +1,6 @@
 'use strict';
 define(function(require) {
-  var angular = require('angular', '');
+  var angular = require('angular');
   return angular.module('gemtc.resources', ['ngResource'])
     .factory('ModelResource', require('gemtc-web/resources/modelResource'))
     .factory('ProblemResource', require('gemtc-web/resources/problemResource'))
