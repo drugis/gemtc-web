a2d8be639059cc01f012743ce6b1d5cd69c71542
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2016-04-22 17:24:02 +0200

    Use Patavi listener service verbatim

--- app/js/services.js
+++ app/js/services.js
@@ -2,7 +2,6 @@
 define(function(require) {
   var angular = require('angular');
   return angular.module('gemtc.services', [])
-    .factory('PataviService', require('gemtc-web/services/pataviService'))
     .factory('RelativeEffectsTableService', require('gemtc-web/services/relativeEffectsTableService'))
     .factory('DevianceStatisticsService', require('gemtc-web/models/devianceStatisticsService'))
     .factory('ModelService', require('gemtc-web/models/modelService'))
