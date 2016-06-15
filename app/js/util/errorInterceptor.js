58ad4af49ffd5a655675ab8a53c988934a6e5628
Connor Stroomberg <c.stroomberg@gmail.com>
2016-06-15 11:49:51 +0200

    work around to suppress patavi warmup request

--- app/js/util/errorInterceptor.js
+++ app/js/util/errorInterceptor.js
@@ -1,40 +1,46 @@
 'use strict';
 define(['angular'], function() {
-	var dependencies = ['$q', '$rootScope'];
-	var ErrorInterceptor = function($q, $rootScope) {
+  var dependencies = ['$q', '$rootScope'];
+  var ErrorInterceptor = function($q, $rootScope) {
 
-		function handleReaction(rejection) {
-			var data, message;
-			if (rejection && rejection.status === 404) {
-				$rootScope.$broadcast('error', {
-					code: 404,
-					cause: rejection.data.error
-				});
-				return $q.reject(rejection);
-			} else if (rejection && rejection.data && rejection.data !== "") {
-				data = rejection.data;
-				message = {
-					code: data.code,
-					cause: data.message
-				};
-			} else {
-				message = {
-					cause: 'An unknown error occurred'
-				};
-			}
+    function handleReaction(rejection) {
+      var data, message;
 
-			$rootScope.$broadcast('error', message);
-			return $q.reject(rejection);
-		}
+      // dirty work around to swallow error on patavi warm-up request, needs to be replaced in new error handeling
+      if (rejection && rejection.config && rejection.config.url.indexOf('warm-up', rejection.config.url.length - rejection.config.url.length) !== -1) {
+        return $q.resolve();
+      }
 
-		return {
-			'requestError': function(rejection) {
-				return handleReaction(rejection);
-			},
-			'responseError': function(rejection) {
-				return handleReaction(rejection);
-			}
-		};
-	};
-	return dependencies.concat(ErrorInterceptor);
+      if (rejection && rejection.status === 404) {
+        $rootScope.$broadcast('error', {
+          code: 404,
+          cause: rejection.data.error
+        });
+        return $q.reject(rejection);
+      } else if (rejection && rejection.data && rejection.data !== "") {
+        data = rejection.data;
+        message = {
+          code: data.code,
+          cause: data.message
+        };
+      } else {
+        message = {
+          cause: 'An unknown error occurred'
+        };
+      }
+
+      $rootScope.$broadcast('error', message);
+      return $q.reject(rejection);
+    }
+
+    return {
+      'requestError': function(rejection) {
+        return handleReaction(rejection);
+      },
+      'responseError': function(rejection) {
+        return handleReaction(rejection);
+      }
+    };
+  };
+  return dependencies.concat(ErrorInterceptor);
 });
