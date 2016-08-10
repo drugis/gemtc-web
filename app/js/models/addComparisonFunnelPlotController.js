74a5f9748162cac4caf76d0fb55c7c21280c1f09
Daan Reid <osmosisch@gmail.com>
2016-08-10 17:24:44 +0200

    Add funnel plot logic/frontend

---
+++ app/js/models/addComparisonFunnelPlotController.js
@@ -0,0 +1,52 @@
+'use strict';
+define(['lodash'], function(_) {
+  var dependencies = ['$scope', '$modalInstance', '$stateParams', 'FunnelPlotResource', 'problem', 'successCallback'];
+  var AddComparisonFunnelPlotController = function($scope, $modalInstance, $stateParams, FunnelPlotResource, problem, successCallback) {
+    $scope.comparisons = buildComparisons();
+    $scope.savePlot = savePlot;
+    $scope.cancel = cancel;
+
+    function buildComparisons() {
+      var comparisons = [];
+      problem.treatments.forEach(function(treatment, idx) {
+        var restOfTreatments = problem.treatments.slice(idx + 1);
+        restOfTreatments.forEach(function(otherTreatment) {
+          comparisons.push({
+            t1: treatment,
+            t2: otherTreatment
+          });
+        });
+      });
+      return comparisons;
+    }
+
+    function cancel() {
+      $modalInstance.dismiss('cancel');
+    }
+
+    function buildPlotObject(comparisons) {
+      var includedComparisons = _.filter(comparisons, 'isIncluded');
+      return {
+        includedComparisons: includedComparisons.map(function(comparison) {
+          return {
+            t1: comparison.t1.id,
+            t2: comparison.t2.id
+          };
+        })
+      };
+    }
+
+    function savePlot() {
+      $scope.isSaving = true;
+
+      FunnelPlotResource
+        .save($stateParams, buildPlotObject($scope.comparisons))
+        .$promise.then(function() {
+          successCallback();
+          $modalInstance.close();
+        });
+    }
+
+  };
+  return dependencies.concat(AddComparisonFunnelPlotController);
+});
