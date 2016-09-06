d874f19efe1dcc908e08b16a324f25c63da449ae
Daan Reid <osmosisch@gmail.com>
2016-09-06 16:57:45 +0200

    bugfix

--- app/js/models/funnelPlot/funnelPlotDirective.js
+++ app/js/models/funnelPlot/funnelPlotDirective.js
@@ -88,7 +88,7 @@ define(['d3', 'nvd3', 'lodash'], function(d3, nvd3, _) {
             var myData = [{
               values: results.studyRelativeEffects.mean.map(function(meanVal, idx) {
                 return {
-                  x: consistentDirection(meanVal, results.studyRelativeEffects.t1[idx], results.studyRelativeEffects.t1[idx]),
+                  x: consistentDirection(meanVal, results.studyRelativeEffects.t1[idx], results.studyRelativeEffects.t2[idx]),
                   y: results.studyRelativeEffects['std.err'][idx]
                 };
               })
