333339f343ade6f7bcfe919eebefca0e4db684d1
Daan Reid <osmosisch@gmail.com>
2016-09-16 10:07:47 +0200

    spec

---
+++ app/js/models/result/resultsPlotServiceSpec.js
@@ -0,0 +1,38 @@
+'use strict';
+define(['angular', 'angular-mocks', 'services'], function(angular) {
+  describe('the result plots service', function() {
+
+    var resultsPlotsService;
+    beforeEach(function() {
+      module('gemtc.services');
+    });
+    beforeEach(inject(function(ResultsPlotService) {
+      resultsPlotsService = ResultsPlotService;
+    }));
+
+    describe('prefixImageUris', function() {
+      it('should add the resultplot prefix to each href', function() {
+        var plotObj = {
+          plot1: {
+            href: 'test.jpg',
+            otherProp: 'something'
+          },
+          plot2: {
+            href: 'othertest.svg',
+            someProp: 'itsathing'
+          }
+        };
+        var prefix = 'https://patavi.org/tasks/32/results/';
+
+        var expectedPlotObj = angular.copy(plotObj);
+        expectedPlotObj.plot1.href = prefix + expectedPlotObj.plot1.href;
+        expectedPlotObj.plot2.href = prefix + expectedPlotObj.plot2.href;
+
+        expect(resultsPlotsService.prefixImageUris(plotObj, prefix)).toEqual(expectedPlotObj);
+
+      });
+    });
+
+  });
+
+});
