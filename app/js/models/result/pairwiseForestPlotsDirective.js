39534ca84a1f95c661de966e0de2ff98378e0428
Daan Reid <osmosisch@gmail.com>
2016-09-20 16:49:51 +0200

    fix pairwise forest plot

--- app/js/models/result/pairwiseForestPlotsDirective.js
+++ app/js/models/result/pairwiseForestPlotsDirective.js
@@ -15,8 +15,14 @@ define([], function() {
         $q.all([scope.modelPromise, scope.resultsPromise, scope.problemPromise])
         .then(function(modelResultProblem) {
           scope.model = modelResultProblem[0];
-          scope.results = modelResultProblem[1].results;
+          scope.results = modelResultProblem[1];
           scope.problem = modelResultProblem[2];
+          scope.results.studyForestPlot = _.map(scope.results.studyForestPlot, function(page) {
+            return {
+              href: scope.model.taskUrl + '/results/' + page.href,
+              'content-type': page['content-type']
+            };
+          });
         });
 
       }
