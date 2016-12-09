0c3a214ae53099ed92e75d60b43fd6cc9570889f
Daan Reid <osmosisch@gmail.com>
2016-12-09 22:28:34 +0100

    try to export with cors

--- app/js/util/exportDirective.js
+++ app/js/util/exportDirective.js
@@ -74,6 +74,7 @@ define(['lodash', 'd3', 'jQuery'], function(_, d3, jQuery) {
             .attr('width', $svgElement.width())
             .attr("version", 1.1)
             .attr("xmlns", "http://www.w3.org/2000/svg")
+            .attr('crossOrigin', 'anonymous')
             .parent()[0]
             .innerHTML;
 
