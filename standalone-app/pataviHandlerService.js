4531d8e17976c532192764a5452279c8b02e1107
Daan Reid <osmosisch@gmail.com>
2016-12-09 21:21:15 +0100

    redundant code

--- standalone-app/pataviHandlerService.js
+++ standalone-app/pataviHandlerService.js
@@ -30,27 +30,6 @@ function createPataviTask(analysis, model, callback) {
   pataviTaskRepository.create(problemPlusModelSettings, callback);
 }
 
-function filterPairwiseTreatments(treatments, fromName, toName) {
-  return _.filter(treatments, function(treatment) {
-    return treatment.name === fromName || treatment.name === toName;
-  });
-}
-
-function filterPairwiseEntries(entries, treatments) {
-  return _.filter(entries, function(entry) {
-    return entry.treatment === treatments[0].id ||
-      entry.treatment === treatments[1].id;
-  });
-}
-
-function reduceToPairwiseProblem(problem) {
-  problem.treatments = filterPairwiseTreatments(problem.treatments,
-    problem.modelType.details.from.name,
-    problem.modelType.details.to.name);
-  problem.entries = filterPairwiseEntries(problem.entries, problem.treatments);
-  return problem;
-}
-
 function leaveStudyOut(problemPlusSettings) {
   problemPlusSettings.entries = _.filter(problemPlusSettings.entries, function(entry) {
     return entry.study !== problemPlusSettings.sensitivity.omittedStudy;
