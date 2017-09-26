fc769c000bf30d3ff913d7403160807202ff1f46
Joris de Keijser <chinook25@gmail.com>
2017-09-26 13:47:46 +0200

    foundation upgrade wip

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 define(
   ['angular',
+    'angular-touch',
     'require',
     'jQuery',
     'lodash',
@@ -25,8 +26,7 @@ define(
     var dependencies = [
       'ui.router',
       'ngSanitize',
-      'mm.foundation.tpls',
-      'mm.foundation.modal',
+      'mm.foundation',
       'gemtc.controllers',
       'gemtc.constants',
       'gemtc.services',
