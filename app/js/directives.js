e32685d8b95be7f4e366652ffbe58efba1b39538
Joris de Keijser <chinook25@gmail.com>
2017-10-09 09:48:47 +0200

    foundation upgrade wip

--- app/js/directives.js
+++ app/js/directives.js
@@ -7,7 +7,6 @@ define(function(require) {
     .directive('gemtcRankPlot', require('gemtc-web/models/rankPlotDirective'))
     .directive('pagedSvgPlot', require('gemtc-web/util/pagedSvgPlotDirective'))
     .directive('pagedPngPlot', require('gemtc-web/util/pagedPngPlotDirective'))
-    .directive('appAlert', require('gemtc-web/util/appAlertDirective'))
     .directive('runLength', require('gemtc-web/models/runLength/runLengthDirective'))
     .directive('nodesplitForestPlot', require('gemtc-web/models/nodesplitForestPlot/nodesplitForestPlotDirective'))
     .directive('heterogeneityPrior', require('gemtc-web/models/heterogeneityPrior/heterogeneityPriorDirective'))
