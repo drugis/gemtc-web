e13515045d3c4b8ea45d92a52ac77cbc3262ce00
Joris de Keijser <chinook25@gmail.com>
2018-09-10 11:40:09 +0200

    add nvd3 css

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
-define(['angular', 'app'], function () {
-  'use strict';
+'use strict';
+define(['angular', 'app'], function() {
   require('css/gemtc-drugis.css');
-  require('font-awesome/css/font-awesome.min.css'); 
+  require('font-awesome/css/font-awesome.min.css');
+  require('nvd3/build/nv.d3.min.css');
 });
