367e1769b3ac8169dcbf1d2164b4d7f9733b2427
Joris de Keijser <chinook25@gmail.com>
2018-10-05 16:04:39 +0200

    add sleep and remove file to db script

--- setup-db.sh
+++ setup-db.sh
@@ -1,5 +1,7 @@
 docker run --name postgres -e POSTGRES_PASSWORD=develop -d postgres
 for i in changesets/create-database-changeset-*.sql; do cat $i >> db-init.sql; done
+sleep 3
 docker run -i -v `pwd`:`pwd` -w `pwd` --rm --link postgres:postgres postgres psql -h postgres -U postgres \
   -c "CREATE USER gemtc WITH PASSWORD 'develop'" -c "CREATE DATABASE gemtc ENCODING 'utf-8' OWNER gemtc" \
   -c '\c gemtc gemtc' -f db-init.sql
+rm db-init.sql
