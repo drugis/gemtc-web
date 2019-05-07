b4038af8ef9fbe6cd020f7e5ddea76901725db13
Joris de Keijser <chinook25@gmail.com>
2019-05-07 11:33:07 +0200

    add cascade to db

--- liquibase/liquibase-changelog.sql
+++ liquibase/liquibase-changelog.sql
@@ -138,3 +138,18 @@ ALTER TABLE account ADD COLUMN password VARCHAR DEFAULT '';
 --changeset reidd:16
 ALTER TABLE account DROP COLUMN name;
 
+--changeset keijserj:17
+START TRANSACTION;
+ALTER TABLE funnelplot ADD CONSTRAINT funnelplot_modelid_fkey FOREIGN KEY (modelId) REFERENCES model(id) ON DELETE CASCADE;
+ALTER TABLE modelBaseline DROP CONSTRAINT modelbaseline_modelid_fkey;
+ALTER TABLE modelBaseline ADD CONSTRAINT modelbaseline_modelid_fkey FOREIGN KEY (modelId) REFERENCES model(id) ON DELETE CASCADE;
+ALTER TABLE model DROP CONSTRAINT model_analysisid_fkey;
+ALTER TABLE model ADD CONSTRAINT model_analysisid_fkey FOREIGN KEY (analysisId) REFERENCES analysis(id) ON DELETE CASCADE;
+COMMIT;
+--rollback START TRANSACTION;
+--rollback ALTER TABLE DROP CONSTRAINT funnelplot_modelid_fkey;
+--rollback ALTER TABLE modelBaseline DROP CONSTRAINT modelbaseline_modelid_fkey;
+--rollback ALTER TABLE modelBaseline ADD CONSTRAINT FOREIGN KEY (modelId) REFERENCES model(id);
+--rollback ALTER TABLE model DROP CONSTRAINT model_analysisid_fkey;
+--rollback ALTER TABLE model ADD CONSTRAINT FOREIGN KEY (analysisId) REFERENCES analysis(id);
+--rollback COMMIT;
