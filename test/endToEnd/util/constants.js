2e6c853b1a7edc2488a0d584b85bb44c0e45ca9c
karlis <karlis.zalite@gmail.com>
2020-02-07 09:05:00 +0100

    add constants file

---
+++ test/endToEnd/util/constants.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  MODEL_TITLE: 'title',
+  ANALYSIS_TITLE: 'my title',
+  OUTCOME: 'my outcome',
+  MODEL_WAIT_TIME_OUT: 15000,
+  DEFAULT_ANALYSIS: '/example.json',
+  TEST_URL: 'http://localhost:3001'
+};
