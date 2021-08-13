aac4750a67588386b8d8bfd1c75d10c1fb882906
Joris <chinook25@gmail.com>
2021-08-13 14:41:09 +0200

    forgot env for run script

--- run-gemtc-docker.sh
+++ run-gemtc-docker.sh
@@ -14,4 +14,6 @@ docker run -d \
  -e GEMTC_DB_PASSWORD=develop \
  -e GEMTC_AUTHENTICATION_METHOD=$ATHENTICATION_METHOD \
  -e PATAVI_HOST=localdocker \
+ -e SECURE_TRAFFIC=false \ 
+ -e PATAVI_API_KEY=someToken \
   addis/gemtc
