37f91528fa82fac4178265ea2a5f35d2edf37008
Daan Reid <osmosisch@gmail.com>
2021-08-30 10:44:30 +0200

    fix tests

--- test/pataviTaskRepositorySpec.js
+++ test/pataviTaskRepositorySpec.js
@@ -11,25 +11,28 @@ var httpsStub = {};
 
 var pataviTaskRepository;
 
-describe('the patavi task repository', function() {
-  beforeEach(function() {
-    pataviTaskRepository = proxyquire('../standalone-app/pataviTaskRepository', {
-      'https': httpsStub
-    });
+describe('the patavi task repository', function () {
+  beforeEach(function () {
+    pataviTaskRepository = proxyquire(
+      '../standalone-app/pataviTaskRepository',
+      {
+        https: httpsStub
+      }
+    );
   });
-  describe('getResult', function() {
-    it('should get the result', function(done) {
+  describe('getResult', function () {
+    it('should get the result', function (done) {
       var pataviResult = {
         status: 'done',
         _links: {
           results: {
-            href: 'resultsLink'
+            href: 'http://resultsLink.com'
           }
         }
       };
       var resultsResult = {};
 
-      var callback = function(error) {
+      var callback = function (error) {
         chaiExpect(error).to.equal(null);
         httpsStub.get.restore();
         done();
@@ -37,12 +40,12 @@ describe('the patavi task repository', function() {
       var getStub = sinon.stub(httpsStub, 'get');
       stubHttpResponse(getStub, 0, pataviResult);
       stubHttpResponse(getStub, 1, resultsResult);
-      pataviTaskRepository.getResult('123', callback);
+      pataviTaskRepository.getResult('http://123.com', callback);
     });
 
-    it('should return an error when no results are present', function(done) {
+    it('should return an error when no results are present', function (done) {
       var responseFromTaskQuery = buildHttpResponse({});
-      var callback = function(error) {
+      var callback = function (error) {
         chaiExpect(error).to.eql({
           description: 'no result found'
         });
@@ -52,30 +55,33 @@ describe('the patavi task repository', function() {
       var getStub = sinon.stub(httpsStub, 'get');
       getStub.onCall(0).yields(responseFromTaskQuery);
       getStub.onCall(0).returns({
-        on: function() {}
+        on: function () {}
       });
-      pataviTaskRepository.getResult('123', callback);
+      pataviTaskRepository.getResult('http://123.com', callback);
     });
   });
-  describe('getPataviTasksStatus', function() {
-    it('should get the task status for the given uris', function(done) {
-      var url1 = 'url1',
-        url2 = 'url2';
+  describe('getPataviTasksStatus', function () {
+    it('should get the task status for the given uris', function (done) {
+      var url1 = 'http://url1.com',
+        url2 = 'http://url2.com';
       var urls = [url1, url2];
-      var expectedResult = [{
-        id: url1,
-        runStatus: 'done'
-      }, {
-        id: url2,
-        runStatus: undefined
-      }];
+      var expectedResult = [
+        {
+          id: url1,
+          runStatus: 'done'
+        },
+        {
+          id: url2,
+          runStatus: undefined
+        }
+      ];
 
-      var expectationsCallback = function(err, result) {
+      var expectationsCallback = function (err, result) {
         chaiExpect(err).to.equal(null);
         chaiExpect(result).to.deep.equal(expectedResult);
         httpsStub.get.restore();
         done();
-      }
+      };
 
       var doneTaskResponse = {
         status: 'done'
@@ -88,10 +94,10 @@ describe('the patavi task repository', function() {
       pataviTaskRepository.getPataviTasksStatus(urls, expectationsCallback);
     });
   });
-  describe('create', function() {
-    it('should post the problem to patavi, and return the task url from the response location', function(done) {
+  describe('create', function () {
+    it('should post the problem to patavi, and return the task url from the response location', function (done) {
       var problem = {};
-      var checkResponseAndCleanup = function(error, createdurl) {
+      var checkResponseAndCleanup = function (error, createdurl) {
         chaiExpect(error).to.equal(null);
         chaiExpect(createdurl).to.equal('createdUrl');
         httpsStub.request.restore();
@@ -103,17 +109,24 @@ describe('the patavi task repository', function() {
           location: 'createdUrl'
         }
       };
-      sinon.stub(httpsStub, 'request')
-        .onCall(0).yields(response)
-        .onCall(0).returns({write: function(){}, end: function(){}});
-      pataviTaskRepository.create(problem, 'service=gemtc', checkResponseAndCleanup);
+      sinon
+        .stub(httpsStub, 'request')
+        .onCall(0)
+        .yields(response)
+        .onCall(0)
+        .returns({write: function () {}, end: function () {}});
+      pataviTaskRepository.create(
+        problem,
+        'service=gemtc',
+        checkResponseAndCleanup
+      );
     });
   });
 
-  describe('deleteTask', function() {
-    it('should delete the task', function(done) {
-      var taskId = 'taskId';
-      var checkResponseAndCleanup = function(error) {
+  describe('deleteTask', function () {
+    it('should delete the task', function (done) {
+      var taskId = 'http://taskId.com';
+      var checkResponseAndCleanup = function (error) {
         chaiExpect(error).to.equal(null);
         httpsStub.request.restore();
         done();
@@ -121,9 +134,12 @@ describe('the patavi task repository', function() {
       var response = {
         statusCode: 200
       };
-      sinon.stub(httpsStub, 'request')
-        .onCall(0).yields(response)
-        .onCall(0).returns({end: function(){}});
+      sinon
+        .stub(httpsStub, 'request')
+        .onCall(0)
+        .yields(response)
+        .onCall(0)
+        .returns({end: function () {}});
       pataviTaskRepository.deleteTask(taskId, checkResponseAndCleanup);
     });
   });
@@ -135,14 +151,14 @@ function stubHttpResponse(methodStub, callNumber, resultObject) {
   var response = buildHttpResponse(resultObject);
   methodStub.onCall(callNumber).yields(response);
   methodStub.onCall(callNumber).returns({
-    on: function() {}
+    on: function () {}
   });
 }
 
 function buildHttpResponse(resultObject) {
   return {
-    setEncoding: function() {},
-    on: function(eventName, callback) {
+    setEncoding: function () {},
+    on: function (eventName, callback) {
       if (eventName === 'data') {
         callback(JSON.stringify(resultObject));
       } else if (eventName === 'end') {
